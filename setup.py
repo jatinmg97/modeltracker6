f119975fbb1a3d50f94774ace1dc954106c86481
jatinmg97 <44123095+jatinmg97@users.noreply.github.com>
2019-09-06 11:34:18 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
   author = 'Miracle',                   # Type in your name
   author_email = 'jgongiwala@miraclesoft.com',      # Type in your E-Mail
   url = 'https://github.com/user/reponame',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/jatinmg97/modeltracker6/archive/v_1.zip',    # I explain this later on
+  download_url = 'https://github.com/jatinmg97/modeltracker6/archive/v_1.tar.gz',    # I explain this later on
   keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'validators',
